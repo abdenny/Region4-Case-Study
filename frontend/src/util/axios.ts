9f0ea96e17b24391b22f19d4606d5ede4bdf0302
Austin Denny <austinbdenny@gmail.com>
2023-10-06 12:11:51 -0500

    style changes and test workflow

--- frontend/src/util/axios.ts
+++ frontend/src/util/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.coincap.io/v2/',
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json',
   timeout: 10000,
